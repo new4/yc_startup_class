1e82f3c5d7d5cd85540aebb38c17188f2a9fedab
new4 <new4wang@163.com>
2019-01-28 18:10:53 +0800

    little change

--- bin/download.js
+++ bin/download.js
@@ -14,31 +14,19 @@ request(URL)
     console.log(response.statusCode); // 200
     console.log(prettyBytes(response.headers['content-length'], 5, false)); // 'image/png'
 
-    // response.on('data', (data) => {
-    //   console.log('received ' + data.length + ' bytes of compressed data')
-    // })
     const len = parseInt(response.headers['content-length'], 10);
 
     console.log();
-    const bar = new ProgressBar('  downloading [:bar] :rate/bps :percent :etas :current/:total', {
+    const bar = new ProgressBar('  downloading [:bar] :percent :etas', {
       complete: '=',
       incomplete: ' ',
-      width: 20,
+      width: 60,
       total: len,
     });
 
-    response.on('data', function (chunk) {
-      bar.tick(chunk.length);
-    });
-
-    response.on('end', function () {
-      console.log('\n');
-    });
+    response.on('data', chunk => bar.tick(chunk.length));
+    response.on('end', () => console.log('response end \n'));
   })
-  // .on('data', (data) => {
-  //   console.log('@@####');
-  //   console.log(data.length); // 200
-  // })
   .on('error', err => console.log(err))
   .pipe(fs.createWriteStream('asd.mp4'))
   .on('finish', () => console.log('视频下载成功'));
