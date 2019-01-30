2123f9ef0dfc772a0535747b8cc795c5393c8e37
new4 <new4wang@163.com>
2019-01-30 20:22:56 +0800

    refactor(add myTest): 使用缓存的信息，调试的时候方便

--- scripts/download.js
+++ scripts/download.js
@@ -4,21 +4,29 @@ const ProgressBar = require('progress');
 
 const {
   log: {
-    logBefore,
+    log,
     logAfter,
+    successlogBoth,
+    faillogBoth,
   },
   colorStr: {
+    grey,
     cyan,
     yellow,
   },
   prettyBytes,
 } = require('@new4/utils');
 
-module.exports = (name, downloadUrl) => new Promise((resolve, reject) => {
+const DIR = 'video';
+
+module.exports = (name, downloadUrl) => new Promise((resolve) => {
+  let failed = false;
+  const videoFile = `${DIR}/${name}.mp4`;
   request(downloadUrl)
     .on('response', (response) => {
       const respLength = response.headers['content-length'];
-      logBefore(cyan(`下载 ${yellow(name)}`));
+      log(grey('-'.repeat(70)));
+      log(cyan(`下载 ${yellow(name)}`));
       logAfter(cyan(`大小 ${yellow(prettyBytes(respLength, 5))}`));
       const bar = new ProgressBar('  :bar :percent', {
         complete: '\u001b[102m \u001b[0m', // Bright Green
@@ -26,9 +34,29 @@ module.exports = (name, downloadUrl) => new Promise((resolve, reject) => {
         width: 60,
         total: parseInt(respLength, 10),
       });
+
       response.on('data', chunk => bar.tick(chunk.length));
+
+      response.on('end', () => {
+        if (!response.complete) {
+          bar.terminate();
+          failed = true;
+        }
+      });
+
+      response.on('error', () => {
+        bar.terminate();
+        failed = true;
+      }); // 懒...
     })
-    .on('error', () => reject()) // 懒...
-    .pipe(fs.createWriteStream(`video/${name}.mp4`))
-    .on('finish', () => resolve()); // 懒...
+    .pipe(fs.createWriteStream(videoFile))
+    .on('finish', () => {
+      if (failed) {
+        fs.unlinkSync(videoFile);
+        faillogBoth('下载失败，删除无效文件，稍后重新下载');
+      } else {
+        successlogBoth(`${yellow(name)} 下载成功!`);
+      }
+      resolve(failed);
+    }); // 懒...
 });
